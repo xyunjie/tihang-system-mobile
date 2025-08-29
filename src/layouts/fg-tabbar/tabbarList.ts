453742b53907e3fec2695ed2e1f7a8d990afd74a
Accepted <xuyunjie@lingman.cn>
2025-08-29 17:56:15 +0800

    refactor(about): 删除关于页面及相关资源，简化项目结构
    - 移除关于页面及其路由配置
    - 删除关于页面的请求和上传等子组件
    - 从页面配置和tabbar列表中移除关于页面项
    - 优化应用结构，减少冗余页面和资源

--- src/layouts/fg-tabbar/tabbarList.ts
+++ src/layouts/fg-tabbar/tabbarList.ts
@@ -32,15 +32,6 @@ export const tabbarList: TabBar['list'] = [
     iconType: 'uiLib',
   },
   {
-    iconPath: 'static/tabbar/example.png',
-    selectedIconPath: 'static/tabbar/exampleHL.png',
-    pagePath: 'pages/about/about',
-    text: '关于',
-    icon: 'i-carbon-code',
-    // 注意 unocss 的图标需要在 页面上引入一下，或者配置到 unocss.config.ts 的 safelist 中
-    iconType: 'unocss',
-  },
-  {
     iconPath: 'static/tabbar/personal.png',
     selectedIconPath: 'static/tabbar/personalHL.png',
     pagePath: 'pages/profile/index',
